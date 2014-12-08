4aafc8be7dc47eee830a39b49037f2832a836f81
Zach Motsinger <zam2007@ku.edu>
2014-12-08 16:36:25 -0600

    cleaning

--- DnD_SQL_Connector.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-
-
-echo "<form action=\"queryResult.php\" method=\"get\">
-	  MySQL Command: <input type = \"Text\" Name = \"phpQuery\" style = \"width: 80%\">
-	  <input type=\"submit\" value=\"Query\">";
-?>
-
